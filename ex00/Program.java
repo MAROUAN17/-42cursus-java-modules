2ab2594a1a065e2bc1f252f598249343b4bd0b29
MAROUAN17 <marinosaglagal25@gmail.com>
2025-06-23 17:38:42 +0100

    ex00

---
+++ ex00/Program.java
@@ -0,0 +1,18 @@
+
+class Program {
+    public int sum = 0;
+    public int calculateSum(int i) {
+        while (i != 0) {
+            sum += (i % 10);
+            i /= 10;
+        }
+        return (sum);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Program p = new Program();
+        System.out.println(p.calculateSum(479598));
+    }
+}
